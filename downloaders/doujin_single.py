0f75492775cc735e8717fb24e309ec8819db6019
YofaGh <ghadiriyosef123@gmail.com>
2023-03-14 12:34:44 +0330

    Added textwrap for long titles

--- downloaders/doujin_single.py
+++ downloaders/doujin_single.py
@@ -1,4 +1,4 @@
-import time, sys, os
+import textwrap, time, sys, os
 from termcolor import colored
 from utils import assets
 
@@ -8,14 +8,15 @@ def download_doujin(code, source, sleep_time, auto_merge, convert_to_pdf):
         try:
             sys.stdout.write(f'\r{code}: Getting name of doujin...')
             doujin_title = source.get_title(code)
-            sys.stdout.write(f'\r{doujin_title}: Creating folder...')
+            shorten_doujin_title = textwrap.shorten(doujin_title, width=50)
+            sys.stdout.write(f'\r{shorten_doujin_title}: Creating folder...')
             fixed_doujin_name = assets.fix_name_for_folder(doujin_title)
             assets.create_folder(fixed_doujin_name)
-            sys.stdout.write(f'\r{doujin_title}: Getting image links...')
+            sys.stdout.write(f'\r{shorten_doujin_title}: Getting image links...')
             images = source.get_images(code)
             adder = 0
             for i in range(len(images)):
-                sys.stdout.write(f'\r{doujin_title}: Downloading image {i+adder+1}/{len(images)+adder}...')
+                sys.stdout.write(f'\r{shorten_doujin_title}: Downloading image {i+adder+1}/{len(images)+adder}...')
                 save_path = f'{fixed_doujin_name}/{i+adder+1:03d}.{images[i].split(".")[-1]}'
                 if not os.path.exists(save_path):
                     time.sleep(sleep_time)
@@ -28,17 +29,17 @@ def download_doujin(code, source, sleep_time, auto_merge, convert_to_pdf):
                         last_truncated = save_path
                         os.remove(save_path)
                         raise Exception('truncated')
-            print(colored(f'\r{doujin_title}: done downloading, {len(images)} images were downloaded.', 'green'))
+            print(colored(f'\r{shorten_doujin_title}: done downloading, {len(images)} images were downloaded.', 'green'))
             if auto_merge:
                 from utils.image_merger import merge_folder
-                sys.stdout.write(f'\r{doujin_title}: Merging...')
+                sys.stdout.write(f'\r{shorten_doujin_title}: Merging...')
                 images_path, lists_to_merge = merge_folder(fixed_doujin_name, f'Merged/{fixed_doujin_name}')
-                print(colored(f'\r{doujin_title}: Merged {len(images_path)} images into {len(lists_to_merge)}.', 'green'))
+                print(colored(f'\r{shorten_doujin_title}: Merged {len(images_path)} images into {len(lists_to_merge)}.', 'green'))
                 if convert_to_pdf:
                     from utils.pdf_converter import convert_folder
-                    sys.stdout.write(f'\r{doujin_title}: Converting to pdf...')
+                    sys.stdout.write(f'\r{shorten_doujin_title}: Converting to pdf...')
                     convert_folder(f'Merged/{fixed_doujin_name}', f'Merged/{fixed_doujin_name}', f'{fixed_doujin_name}.pdf')
-                    print(colored(f'\r{doujin_title}: Converted to pdf.      ', 'green'))
+                    print(colored(f'\r{shorten_doujin_title}: Converted to pdf.      ', 'green'))
             break
         except Exception as error:
             if 'Connection error' in str(error):
