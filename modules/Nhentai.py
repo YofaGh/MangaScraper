c9cbded786987a1edb5b25e4cba400229e680a31
YofaGh <ghadiriyosef123@gmail.com>
2023-04-06 20:39:19 +0330

    Implemented search function for Nyahentai, Nhentai, Simplyhentai

--- modules/Nhentai.py
+++ modules/Nhentai.py
@@ -19,3 +19,25 @@ class Nhentai(Doujin, Req):
             name = name.replace('t.', '.')
             new_images.append(f'{image.rsplit("/", 1)[0]}/{name}')
         return new_images
+
+    def search(title, sleep_time, absolute=False, limit_page=1000):
+        import time
+        results = []
+        page = 1
+        while page <= limit_page:
+            yield False, page
+            response = Nhentai.send_request(f'https://nhentai.xxx/search?q={title}&page={page}')
+            soup = BeautifulSoup(response.text, 'html.parser')
+            doujins = soup.find_all('div', {'class': 'gallery'})
+            if len(doujins) == 0:
+                break
+            for doujin in doujins:
+                doj = doujin.find('a')
+                ti = doj.find('div', {'class': 'caption'}).contents[0]
+                if absolute and title.lower() not in ti.lower():
+                    continue
+                results.append(f'title: {ti}, code: {doj["href"].split("/")[-2]}')
+            page += 1
+            time.sleep(sleep_time)
+        yield True, results
+        return
