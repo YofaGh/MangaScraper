6f6d39be269c9f2ead8585f5d9ffd3a7d439f479
YofaGh <ghadiriyosef123@gmail.com>
2023-04-18 17:50:10 +0330

    Fixed a bug with Hentaifox

--- modules/Hentaifox.py
+++ modules/Hentaifox.py
@@ -1,9 +1,5 @@
-import time
-from utils.models import Doujin
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
+from utils.models import Doujin
 
 class Hentaifox(Doujin):
     def get_title(code):
@@ -13,29 +9,14 @@ class Hentaifox(Doujin):
         return title
 
     def get_images(code):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
-        service = Service(executable_path='geckodriver.exe', log_path='NUL')
-        browser = webdriver.Firefox(options=options, service=service)
-        browser.get(f'https://hentaifox.com/gallery/{code}/')
-        time.sleep(5)
-        view_all_button = browser.find_element(By.XPATH, "//button[@id='load_all']")
-        view_all_button.click()
-        soup = BeautifulSoup(browser.page_source, 'html.parser')
-        divs = soup.find_all('div', {'class': 'gallery_thumb'})
-        for _ in range(21):
-            if len(divs) != 10:
-                break
-            time.sleep(1)
-            soup = BeautifulSoup(browser.page_source, 'html.parser')
-            divs = soup.find_all('div', {'class': 'gallery_thumb'})
-        images = [div.find('img')['data-src'] for div in divs]
-        new_images = []
-        for image in images:
-            name = image.rsplit('/', 1)[1]
-            name = name.replace('t.', '.')
-            new_images.append(f'{image.rsplit("/", 1)[0]}/{name}')
-        return new_images
+        response = Hentaifox.send_request(f'https://hentaifox.com/gallery/{code}')
+        soup = BeautifulSoup(response.text, 'html.parser')
+        sample = soup.find('div', {'class': 'gallery_thumb'}).find('img')['data-src'] 
+        sample_path = sample.rsplit('/', 1)[0]
+        sample_format = sample.rsplit('.', 1)[1]
+        pages = int(soup.find('span', {'class': 'i_text pages'}).contents[0].split(' ')[-1])
+        images = [f'{sample_path}/{i}.{sample_format}' for i in range(1, pages+1)]
+        return images
 
     def search_by_keyword(keyword, absolute):
         from utils.assets import waiter
