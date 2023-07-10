358e9e17051b52cfee5f023457199d20ba876af6
YofaGh <ghadiriyosef123@gmail.com>
2023-07-10 16:15:58 +0330

    Major improvements to Mangapark

--- modules/Mangapark.py
+++ modules/Mangapark.py
@@ -1,48 +1,30 @@
-import time
+import json
 from bs4 import BeautifulSoup
-from selenium import webdriver
 from utils.models import Manga
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 
 class Mangapark(Manga):
     domain = 'mangapark.to'
 
     def get_chapters(manga):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
-        service = Service(executable_path='geckodriver.exe', log_path='NUL')
-        browser = webdriver.Firefox(options=options, service=service)
-        browser.get(f'https://mangapark.to/title/{manga}')
-        soup = BeautifulSoup(browser.page_source, 'html.parser')
-        WebDriverWait(browser, 60).until(ec.presence_of_element_located((By.XPATH, '//div[@class="flex flex-col border-l rounded-l odd:border-primary even:border-secondary"]')))
-        chapters = [link['href'] for link in soup.find_all('a', {'class': 'link-hover link-primary visited:text-accent'})]
-        buttons = browser.find_elements(By.XPATH, "//button[@class='btn btn-xs  btn-primary mr-2 mt-2 btn-outline']")
-        for button in buttons:
-            button.click()
-            WebDriverWait(browser, 60).until(ec.presence_of_element_located((By.XPATH, '//div[@class="flex flex-col border-l rounded-l odd:border-primary even:border-secondary"]')))
-            soup = BeautifulSoup(browser.page_source, 'html.parser')
-            chapters += [link['href'] for link in soup.find_all('a', {'class': 'link-hover link-primary visited:text-accent'})]
-        ctd = [chapters[0].split('/')[-1]]
-        for i in range(1,len(chapters)):
-            if chapters[i-1].split('-')[-1] != chapters[i].split('-')[-1]:
-                ctd.insert(0, chapters[i].split('/')[-1])
-        return ctd
+        manga = manga.split('-')[0] if '-' in manga else manga
+        data = "{\"query\":\"\\n  query get_content_comicChapterRangeList($select: Content_ComicChapterRangeList_Select) {\\n    get_content_comicChapterRangeList(\\n      select: $select\\n    ) {\\n      reqRange{x y}\\n      missing\\n      pager {x y}\\n      items{\\n        serial \\n        chapterNodes {\\n          \\n  id\\n  data {\\n    \\n\\n  id\\n  sourceId\\n\\n  dbStatus\\n  isNormal\\n  isHidden\\n  isDeleted\\n  isFinal\\n  \\n  dateCreate\\n  datePublic\\n  dateModify\\n  lang\\n  volume\\n  serial\\n  dname\\n  title\\n  urlPath\\n\\n  srcTitle srcColor\\n\\n  count_images\\n\\n  stat_count_post_child\\n  stat_count_post_reply\\n  stat_count_views_login\\n  stat_count_views_guest\\n  \\n  userId\\n  userNode {\\n    \\n  id \\n  data {\\n    \\nid\\nname\\nuniq\\navatarUrl \\nurlPath\\n\\nverified\\ndeleted\\nbanned\\n\\ndateCreate\\ndateOnline\\n\\nstat_count_chapters_normal\\nstat_count_chapters_others\\n\\nis_adm is_mod is_vip is_upr\\n\\n  }\\n\\n  }\\n\\n  disqusId\\n\\n\\n  }\\n\\n          sser_read\\n        }\\n      }\\n\\n    }\\n  }\\n  \",\"variables\":{\"select\":{\"comicId\":__comicId__,\"range\":__range__,\"isAsc\":false}},\"operationName\":\"get_content_comicChapterRangeList\"}"
+        response = Mangapark.send_request('https://mangapark.to/apo/', headers={'content-type': 'application/json'}, data=data.replace('__range__', 'null').replace('__comicId__', str(manga)), method='POST')
+        response_raw = json.loads(response.text)
+        end = response_raw['data']['get_content_comicChapterRangeList']['pager'][0]['x']
+        begin = response_raw['data']['get_content_comicChapterRangeList']['pager'][-1]['y']
+        response = Mangapark.send_request('https://mangapark.to/apo/', headers={'content-type': 'application/json'}, data=data.replace('__range__', f'{{"x":{begin},"y":{end}}}').replace('__comicId__', str(manga)), method='POST')
+        response_raw = json.loads(response.text)
+        items = response_raw['data']['get_content_comicChapterRangeList']['items']
+        chapters = [item['chapterNodes'][0]['data']['urlPath'].split('/')[-1] for item in items[::-1]]
+        return chapters
 
     def get_images(manga, chapter):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
-        service = Service(executable_path='geckodriver.exe', log_path='NUL')
-        browser = webdriver.Firefox(options=options, service=service)
-        browser.get(f'https://mangapark.to/title/{manga}/{chapter}')
-        load_all_images_button = browser.find_elements(By.XPATH, "//button[@class='btn btn-sm btn-info btn-outline btn-block normal-case opacity-80']")[1]
-        load_all_images_button.click()
-        time.sleep(10)
-        soup = BeautifulSoup(browser.page_source, 'html.parser')
-        images = soup.find_all('img', {'class': 'z-10 absolute top-0 right-0 bottom-0 left-0 w-full h-full'})
-        images = [image['src'] for image in images]
+        response = Mangapark.send_request(f'https://mangapark.to/title/{manga}/{chapter}')
+        soup = BeautifulSoup(response.text, 'html.parser')
+        script = soup.find('script', {'id': '__NEXT_DATA__'})
+        data = json.loads(script.text)
+        images_raw = (data['props']['pageProps']['dehydratedState']['queries'][0]['state']['data']['data']['imageSet'])
+        images = [f'{url}?{tail}' for url, tail in zip(images_raw['httpLis'], images_raw['wordLis'])]
         save_names = []
         for i in range(len(images)):
             save_names.append(f'{i+1:03d}.{images[i].split(".")[-1].split("?")[0]}')
@@ -50,40 +32,42 @@ class Mangapark(Manga):
 
     def search_by_keyword(keyword, absolute):
         from contextlib import suppress
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
-        service = Service(executable_path='geckodriver.exe', log_path='NUL')
-        browser = webdriver.Firefox(options=options, service=service)
         page = 1
         prev_page = {}
         while True:
-            browser.get(f'https://mangapark.to/v5x-search?word={keyword}&page={page}')
-            WebDriverWait(browser, 60).until(ec.presence_of_element_located((By.XPATH, '//div[@class="flex group border-b border-b-base-200 pb-5"]')))
-            soup = BeautifulSoup(browser.page_source, 'html.parser')
-            divs = soup.find_all('div', {'class': 'flex group border-b border-b-base-200 pb-5'})
-            if prev_page == divs:
+            data = "{\"query\":\"\\n    query get_content_browse_search($select: ComicSearchSelect) {\\n      get_content_browse_search(\\n        select: $select\\n      ) {\\n        reqPage reqSize reqSort reqWord\\n        paging { total pages page size skip }\\n        items {\\n          \\nid\\ndata {\\n  \\n\\nid\\ndbStatus\\nisNormal\\nisHidden\\nisDeleted\\n\\ndateCreate datePublic dateModify\\ndateUpload dateUpdate\\n\\nname\\nslug\\naltNames\\n\\nauthors\\nartists\\ngenres\\n\\noriginalLanguage\\noriginalStatus\\noriginalInfo\\noriginalPubFrom\\noriginalPubTill\\n\\nreadDirection\\n\\nsummary {\\n  code\\n}\\nextraInfo {\\n  code\\n}\\nurlPath\\n\\nurlCover600\\nurlCover300\\nurlCoverOri\\n\\ndisqusId\\n\\n\\n\\nstat_is_hot\\nstat_is_new\\n\\nstat_count_follow\\nstat_count_review \\nstat_count_post_child \\nstat_count_post_reply\\n\\nstat_count_mylists\\n\\nstat_count_vote\\nstat_count_note\\nstat_count_emotions {\\n  field count\\n}\\nstat_count_statuss {\\n  field count\\n}\\nstat_count_scores {\\n  field count\\n}\\nstat_count_views {\\n  field count\\n}\\n\\nstat_score_avg\\nstat_score_bay\\nstat_score_val\\n\\n\\n\\n\\n\\nchart_count_chapters_all\\nchart_count_chapters_bot\\nchart_count_chapters_usr\\n\\nchart_count_serials_all\\nchart_count_serials_bot\\nchart_count_serials_usr\\n\\nchart_count_langs_all\\nchart_count_langs_bot\\nchart_count_langs_usr\\n\\nchart_max_chapterId\\nchart_max_serial_val\\n\\n\\nchart_count_sources_all\\nchart_count_sources_bot\\nchart_count_sources_usr\\n\\nchart_count_lang_to_chapters {field count}\\nchart_count_lang_to_serials {field count}\\n\\n\\nuserId\\nuserNode {\\n  id \\n  data {\\n    \\nid\\nname\\nuniq\\navatarUrl \\nurlPath\\n\\nverified\\ndeleted\\nbanned\\n\\ndateCreate\\ndateOnline\\n\\nstat_count_chapters_normal\\nstat_count_chapters_others\\n\\nis_adm is_mod is_vip is_upr\\n\\n  }\\n}\\n\\nsser_isPageOwner\\nsser_canEditChap\\nsser_canEditInfo\\nsser_canEditPerm\\nsser_canUploadCh\\n\\n\\n}\\n\\n          max_chapterNode {\\n            \\n  id\\n  data {\\n    \\n\\n  id\\n  sourceId\\n\\n  dbStatus\\n  isNormal\\n  isHidden\\n  isDeleted\\n  isFinal\\n  \\n  dateCreate\\n  datePublic\\n  dateModify\\n  lang\\n  volume\\n  serial\\n  dname\\n  title\\n  urlPath\\n\\n  srcTitle srcColor\\n\\n  count_images\\n\\n  stat_count_post_child\\n  stat_count_post_reply\\n  stat_count_views_login\\n  stat_count_views_guest\\n  \\n  userId\\n  userNode {\\n    \\n  id \\n  data {\\n    \\nid\\nname\\nuniq\\navatarUrl \\nurlPath\\n\\nverified\\ndeleted\\nbanned\\n\\ndateCreate\\ndateOnline\\n\\nstat_count_chapters_normal\\nstat_count_chapters_others\\n\\nis_adm is_mod is_vip is_upr\\n\\n  }\\n\\n  }\\n\\n  disqusId\\n\\n\\n  }\\n\\n          }\\n          sser_followed\\n          sser_lastReadChap {\\n            date\\n            chapterNode {\\n              \\n  id\\n  data {\\n    \\n\\n  id\\n  sourceId\\n\\n  dbStatus\\n  isNormal\\n  isHidden\\n  isDeleted\\n  isFinal\\n  \\n  dateCreate\\n  datePublic\\n  dateModify\\n  lang\\n  volume\\n  serial\\n  dname\\n  title\\n  urlPath\\n\\n  srcTitle srcColor\\n\\n  count_images\\n\\n  stat_count_post_child\\n  stat_count_post_reply\\n  stat_count_views_login\\n  stat_count_views_guest\\n  \\n  userId\\n  userNode {\\n    \\n  id \\n  data {\\n    \\nid\\nname\\nuniq\\navatarUrl \\nurlPath\\n\\nverified\\ndeleted\\nbanned\\n\\ndateCreate\\ndateOnline\\n\\nstat_count_chapters_normal\\nstat_count_chapters_others\\n\\nis_adm is_mod is_vip is_upr\\n\\n  }\\n\\n  }\\n\\n  disqusId\\n\\n\\n  }\\n\\n            }\\n          }\\n        }\\n      }\\n    }\\n    \",\"variables\":{\"select\":{\"word\":\"__keyword__\",\"sort\":null,\"page\":__page__,\"incGenres\":[],\"excGenres\":[],\"origLang\":null,\"oficStatus\":null,\"chapCount\":null}},\"operationName\":\"get_content_browse_search\"}"
+            response = Mangapark.send_request('https://mangapark.to/apo/', method='POST', headers={'content-type': 'application/json'}, data=data.replace('__keyword__', keyword).replace('__page__', str(page)))
+            res_raw = json.loads(response.text)
+            mangas = res_raw['data']['get_content_browse_search']['items']
+            if mangas == prev_page:
                 yield {}
             results = {}
-            for div in divs:
-                ti = div.find('h3', {'class': 'font-bold space-x-1 text-lg line-clamp-2'}).find('a')
-                if absolute and keyword.lower() not in ti.contents[0].lower():
+            for manga in mangas:
+                name = manga['data']['name']
+                url = manga['data']['urlPath'].split('/')[-1]
+                authors, artists, genres, status, summary, latest_chapter = '', '', '', '', '', ''
+                with suppress(Exception): authors = ', '.join(manga['data']['authors'])
+                with suppress(Exception): artists = ', '.join(manga['data']['artists'])
+                with suppress(Exception): genres = ', '.join(manga['data']['genres'])
+                with suppress(Exception): status = manga['data']['originalStatus']
+                with suppress(Exception): summary = manga['data']['summary']['code']
+                with suppress(Exception): latest_chapter = manga['max_chapterNode']['data']['urlPath'].split('/')[-1]
+                if absolute and keyword.lower() not in name.lower():
                     continue
-                latest_chapter, genres = '', ''
-                with suppress(Exception):
-                    genres = div.find('div', {'class': 'flex items-center flex-wrap text-xs opacity-70'}).find_all('a')
-                    genres = ', '.join([genre.find('span').contents[0] for genre in genres])
-                with suppress(Exception):
-                    latest_chapter = div.find('div', {'class': 'flex justify-between text-sm flex-wrap'}).find('a')['href'].split('/')[-1]
-                results[ti.contents[0]] = {
+                results[name] = {
                     'domain': Mangapark.domain,
-                    'url': ti['href'].split('/')[-1],
+                    'url': url,
+                    'authors': authors,
+                    'artists': artists,
                     'genres': genres,
+                    'status': status,
+                    'summary': summary,
                     'latest_chapter': latest_chapter,
                     'page': page
                 }
             yield results
             page += 1
-            prev_page = divs
+            prev_page = mangas
 
     def get_db():
         return Mangapark.search_by_keyword('', False)
@@ -107,3 +91,16 @@ class Mangapark(Manga):
             return f'Chapter {int(new_name):03d}'
         except:
             return f'Chapter {new_name.split(".", 1)[0].zfill(3)}.{new_name.split(".", 1)[1]}'
+
+    def send_request(url, method='GET', headers={}, data={}):
+        import requests
+        from utils.assets import waiter
+        while True:
+            try:
+                response = requests.get(url) if method == 'GET' else requests.post(url, headers=headers, data=data)
+                response.raise_for_status()
+                return response
+            except (requests.exceptions.HTTPError, requests.exceptions.Timeout) as error:
+                raise error
+            except requests.exceptions.RequestException:
+                waiter()
