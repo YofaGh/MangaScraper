b1bdcb77f4882c1dfcabc3a448c16f3f09740edf
YofaGh <ghadiriyosef123@gmail.com>
2023-07-11 17:32:15 +0330

    Removed selenium from Manga18 module

--- modules/Manga18.py
+++ modules/Manga18.py
@@ -1,44 +1,33 @@
 from bs4 import BeautifulSoup
 from utils.models import Manga
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 
 class Manga18(Manga):
     domain = 'manga18.club'
+    headers = {'User-Agent': 'Leech/1051 CFNetwork/454.9.4 Darwin/10.3.0 (i386) (MacPro1%2C1)'}
 
     def get_chapters(manga):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
-        service = Service(executable_path='geckodriver.exe', log_path='NUL')
-        browser = webdriver.Firefox(options=options, service=service)
-        browser.get(f'https://manga18.club/manhwa/{manga}')
-        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        response = Manga18.send_request(f'https://manga18.club/manhwa/{manga}', headers=Manga18.headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
         lis = soup.find('div', {'class': 'chapter_box'}).find_all('li')
         chapters = [li.find('a')['href'].split('/')[-1] for li in lis[::-1]]
         return chapters
 
     def get_images(manga, chapter):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
-        service = Service(executable_path='geckodriver.exe', log_path='NUL')
-        browser = webdriver.Firefox(options=options, service=service)
-        browser.get(f'https://manga18.club/manhwa/{manga}/{chapter}')
-        soup = BeautifulSoup(browser.page_source, 'html.parser')
-        images = soup.find('div', {'class':'chapter_boxImages'}).find_all('img')
-        images = [image['src'].strip() for image in images]
+        import base64
+        response = Manga18.send_request(f'https://manga18.club/manhwa/{manga}/{chapter}', headers=Manga18.headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        script = soup.find(lambda tag:tag.name == 'script' and 'slides_p_path' in tag.text)
+        images = script.text.split('[', 1)[1].split(']', 1)[0][:-1]
+        images = [image.replace('"', '') for image in images.split(',')]
+        images = [base64.b64decode(image).decode('utf-8') for image in images]
         return images, False
 
     def search_by_keyword(keyword, absolute):
         from contextlib import suppress
-        from requests.exceptions import HTTPError
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
-        service = Service(executable_path='geckodriver.exe', log_path='NUL')
-        browser = webdriver.Firefox(options=options, service=service)
         page = 1
         while True:
-            browser.get(f'https://manga18.club/list-manga/{page}?search={keyword}')
-            soup = BeautifulSoup(browser.page_source, 'html.parser')
+            response = Manga18.send_request(f'https://manga18.club/list-manga/{page}?search={keyword}', headers=Manga18.headers)
+            soup = BeautifulSoup(response.text, 'html.parser')
             mangas = soup.find_all('div', {'class': 'col-md-3 col-sm-4 col-xs-6'})
             if len(mangas) == 0:
                 yield {}
