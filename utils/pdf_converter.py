c10847edc8f35bc64b8c4c926f894a4a1f816c89
YofaGh <ghadiriyosef123@gmail.com>
2023-07-29 12:09:41 +0330

    Added logger in pdf_converter

--- utils/pdf_converter.py
+++ utils/pdf_converter.py
@@ -1,19 +1,19 @@
-import img2pdf, sys
+import img2pdf
+from utils.logger import log_over, log
 from utils.assets import validate_folder, create_path, detect_images
-from termcolor import colored
 
 def convert_folder(path_to_source, path_to_destination, pdf_name, name=None):
     name = name if name else path_to_source
     pdf_name = pdf_name if pdf_name.endswith('.pdf') else f'{pdf_name}.pdf'
     if not validate_folder(path_to_source):
-        print(colored(f'\rFailed to convert {path_to_source} because of a corrupted image.', 'red'))
+        log(f'\rFailed to convert {path_to_source} because of a corrupted image.', 'red')
         return
     create_path(path_to_destination)
-    sys.stdout.write(f'\r{name}: Converting to pdf...')
+    log_over(f'\r{name}: Converting to pdf...')
     images_path = detect_images(path_to_source)
     with open(f'{path_to_destination}/{pdf_name}', 'wb') as pdf_file:
         pdf_file.write(img2pdf.convert(images_path))
-    print(colored(f'\r{name}: Converted to pdf.      ', 'green'))
+    log(f'\r{name}: Converted to pdf.      ', 'green')
 
 def convert_bulk(path_to_source, path_to_destination):
     import os
@@ -25,14 +25,14 @@ def convert_bulkone(path_to_source, path_to_destination):
     import os
     sub_folders = os.listdir(path_to_source)
     images = []
-    sys.stdout.write(f'\r{path_to_source}: Detecting images...')
+    log_over(f'\r{path_to_source}: Detecting images...')
     for sub_folder in sub_folders:
         if not validate_folder(f'{path_to_source}/{sub_folder}'):
-            print(colored(f'\rFailed to convert {path_to_source}/{sub_folder} because of a corrupted image.', 'red'))
+            log(f'\rFailed to convert {path_to_source}/{sub_folder} because of a corrupted image.', 'red')
             return
         images_path = detect_images(f'{path_to_source}/{sub_folder}')
         images += images_path
-    sys.stdout.write(f'\r{path_to_source}: Creating pdf...    ')
+    log_over(f'\r{path_to_source}: Creating pdf...    ')
     with open(f'{path_to_destination}/{path_to_source}.pdf', 'wb') as pdf_file:
         pdf_file.write(img2pdf.convert(images))
-    print(colored(f'\r{path_to_source}: Converted all subfolders to one pdf.      ', 'green'))
+    log(f'\r{path_to_source}: Converted all subfolders to one pdf.      ', 'green')
