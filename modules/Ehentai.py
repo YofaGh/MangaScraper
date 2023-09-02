d191bb5f8fe82a666dc1a1777b9017426599c282
YofaGh <ghadiriyosef123@gmail.com>
2023-09-02 11:04:33 +0330

    Fixed a bug with some modules

--- modules/Ehentai.py
+++ modules/Ehentai.py
@@ -4,7 +4,7 @@ from utils.models import Doujin
 class Ehentai(Doujin):
     domain = 'ehentai.to'
     logo = 'https://ehentai.to/favicon.ico'
-    download_images_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
+    download_images_headers = {'Referer': 'https://ehentai.to/'}
 
     def get_title(code):
         response = Ehentai.send_request(f'https://ehentai.to/g/{code}')
