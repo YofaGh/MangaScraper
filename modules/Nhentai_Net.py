c55c8142199665eb0dde4db0516d18ab4a59b402
YofaGh <ghadiriyosef123@gmail.com>
2024-01-23 00:07:00 +0330

    Implemented Nhentai_Net Module

---
+++ modules/Nhentai_Net.py
@@ -0,0 +1,29 @@
+from utils.models import Doujin
+
+class Nhentai_Net(Doujin):
+    domain = 'nhentai.net'
+    logo = 'https://static.nhentai.net/img/logo.090da3be7b51.svg'
+
+    def get_info(code, wait=True):
+        response = Nhentai_Net.send_request(f'https://cubari.moe/read/api/nhentai/series/{code}/', wait=wait).json()
+        images = list(list(response['chapters'].values())[0]['groups'].values())[0]
+        return {
+            'Cover': response['cover'],
+            'Title': response['title'],
+            'Pages': len(images),
+            'Extras': {
+                'Artists': response['artist'],
+                'Authors': response['author'],
+                'Groups': list(response['groups'].values()),
+                'Description': response['description'],
+            },
+        }
+
+    def get_title(code, wait=True):
+        response = Nhentai_Net.send_request(f'https://cubari.moe/read/api/nhentai/series/{code}/', wait=wait).json()
+        return response['title']
+
+    def get_images(code, wait=True):
+        response = Nhentai_Net.send_request(f'https://cubari.moe/read/api/nhentai/series/{code}/', wait=wait).json()
+        images = list(response['chapters'].values())[0]['groups']
+        return list(images.values())[0], False
