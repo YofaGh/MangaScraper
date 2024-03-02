7d43c609b6fbba9ea95a7a870f5df3978a1ef094
YofaGh <ghadiriyosef123@gmail.com>
2024-03-02 17:12:17 +0330

    only run system(color) when on windows

--- cli.py
+++ cli.py
@@ -1,4 +1,4 @@
-import argparse, settings, sys, os
+import argparse, platform, settings, sys, os
 
 class SetChapters(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
@@ -55,7 +55,7 @@ if args.task in ('manga', 'doujin', 'search', 'db') and not args.file:
 if (args.single or args.task == 'db') and len(args.s) > 1:
     parser.error('please specify one module using -s argument.\nyou can only set one module when downloading or getting database.')
 
-os.system('color')
+if platform.system() == 'Windows': os.system('color')
 
 match args.task:
     case 'manga':
