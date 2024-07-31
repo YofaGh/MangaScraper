8885b7beee0bcedb1cd1e61ddeeda44a4ecb352d
NekoChan <aaron74327@gmail.com>
2024-07-30 22:16:04 -0700

    fix: AttributeError: 'tuple' object has no attribute 'json'

--- modules/Nhentai_Net.py
+++ modules/Nhentai_Net.py
@@ -5,25 +5,26 @@ class Nhentai_Net(Doujin):
     logo = 'https://static.nhentai.net/img/logo.090da3be7b51.svg'
 
     def get_info(code):
-        response, _ = Nhentai_Net.send_request(f'https://cubari.moe/read/api/nhentai/series/{code}/').json()
-        images = list(list(response['chapters'].values())[0]['groups'].values())[0]
+        response, _ = Nhentai_Net.send_request(f'https://cubari.moe/read/api/nhentai/series/{code}/')
+        info = response.json()
+        images = list(list(info['chapters'].values())[0]['groups'].values())[0]
         return {
-            'Cover': response['cover'],
-            'Title': response['title'],
+            'Cover': info['cover'],
+            'Title': info['title'],
             'Pages': len(images),
             'Extras': {
-                'Artists': response['artist'],
-                'Authors': response['author'],
-                'Groups': list(response['groups'].values()),
-                'Description': response['description'],
+                'Artists': info['artist'],
+                'Authors': info['author'],
+                'Groups': list(info['groups'].values()),
+                'Description': info['description'],
             },
         }
 
     def get_title(code):
-        response, _ = Nhentai_Net.send_request(f'https://cubari.moe/read/api/nhentai/series/{code}/').json()
-        return response['title']
+        response, _ = Nhentai_Net.send_request(f'https://cubari.moe/read/api/nhentai/series/{code}/')
+        return response.json()['title']
 
     def get_images(code):
-        response, _ = Nhentai_Net.send_request(f'https://cubari.moe/read/api/nhentai/series/{code}/').json()
-        images = list(response['chapters'].values())[0]['groups']
+        response, _ = Nhentai_Net.send_request(f'https://cubari.moe/read/api/nhentai/series/{code}/')
+        images = list(response.json()['chapters'].values())[0]['groups']
         return list(images.values())[0], False
